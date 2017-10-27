aa5e4e2c8adb1c8d8fc7faa26ce854cc5e117cc2
xhuang31 <xhuang31@hawk.iit.edu>
2017-10-27 17:36:03 -0500

    Initial Release

---
+++ Runme.py
@@ -0,0 +1,46 @@
+import numpy as np
+import scipy.io as sio
+from aane_fun import aane_fun
+import time
+
+
+'''################# Load data  #################'''
+mat_contents = sio.loadmat('BlogCatalog.mat')
+lambd = 10**-0.6  # the regularization parameter
+rho = 5  # the penalty parameter
+
+# mat_contents = sio.loadmat('Flickr.mat')
+# lambd = 0.0425  # the regularization parameter
+# rho = 4  # the penalty parameter
+
+'''################# Experimental Settings #################'''
+d = 100  # the dimension of the embedding representation
+G = mat_contents["Network"]
+A = mat_contents["Attributes"]
+Label = mat_contents["Label"]
+del mat_contents
+n = G.shape[0]
+Indices = sio.loadmat('Experiment1.mat')["Indices"]
+# Indices = np.random.randint(25, size=n)  # 5-fold cross-validation indices
+
+Group1 = []
+Group2 = []
+[Group1.append(x) for x in range(0, n) if Indices[x] <= 20]  # 2 for 10%, 5 for 25%, 20 for 100% of training group
+[Group2.append(x) for x in range(0, n) if Indices[x] >= 21]  # test group
+n1 = len(Group1)  # num of nodes in training group
+n2 = len(Group2)  # num of nodes in test group
+CombG = G[Group1+Group2, :][:, Group1+Group2]
+CombA = A[Group1+Group2, :]
+
+'''################# Accelerated Attributed Network Embedding #################'''
+print 'Accelerated Attributed Network Embedding (AANE), 5-fold with 50% of training is used:'
+start_time = time.time()
+h = aane_fun(CombG, CombA, d, lambd, rho)
+print "time elapsed: {:.2f}s".format(time.time() - start_time)
+
+'''################# AANE for a Pure Network #################'''
+print 'AANE for a pure network:'
+start_time = time.time()
+h = aane_fun(CombG, CombG, d, lambd, rho)
+print "time elapsed: {:.2f}s".format(time.time() - start_time)
+sio.savemat('H.mat', {"H": h})
---
+++ aane_fun.py
@@ -0,0 +1,109 @@
+def aane_fun(net, attri, d, *varargs):
+    """Jointly embed net and attri into embedding representation h
+       h = aane_fun(net,attri,d);
+       h = aane_fun(net,attri,d,lambda,rho);
+       h = aane_fun(net,attri,d,lambda,rho,'Att');
+       h = aane_fun(net,attri,d,lambda,rho,'Att',worknum);
+    :param net: the weighted adjacency matrix
+    :param attri: the attribute information matrix with row denotes nodes
+    :param d: the dimension of the embedding representation
+    :param lambd: the regularization parameter
+    :param rho: the penalty parameter
+    :param 'Att': refers to conduct Initialization from the SVD of Attri
+    :param splitnum: the number of pieces we split the SA for limited cache
+    :return: the embedding representation h
+    Copyright 2017, Xiao Huang and Jundong Li.
+    $Revision: 1.0.0 $  $Date: 2017/10/25 00:00:00 $
+    """
+    global affi, sa, h, z
+    import numpy as np
+    from scipy import sparse
+    from scipy.sparse import csc_matrix
+    from scipy.sparse.linalg import svds
+    from math import ceil
+
+    '''################# Parameters #################'''
+    maxiter = 2  # Max num of iteration
+    [n, m] = attri.shape  # n = Total num of nodes, m = attribute category num
+    net = sparse.lil_matrix(net)
+    net.setdiag(np.zeros(n))
+    net = csc_matrix(net)
+    attri = csc_matrix(attri)
+    lambd = 0.1  # Initial regularization parameter
+    rho = 5  # Initial penalty parameter
+    splitnum = 1  # number of worker used for distribution
+    if len(varargs) > 0:
+        lambd = varargs[0]
+        rho = varargs[1]
+        if len(varargs) >=3 and varargs[2] == 'Att':
+            sumcol = attri.sum(0)
+            h = svds(attri[:, sorted(range(m), key=lambda k: sumcol[0, k], reverse=True)[0:min(10 * d, m)]], d)[0]
+        else:
+            sumcol = net.sum(0)
+            h = svds(net[:, sorted(range(n), key=lambda k: sumcol[0, k], reverse=True)[0:min(10 * d, n)]], d)[0]
+        if len(varargs) >=4:
+            splitnum = varargs[3]
+    else:
+        sumcol = net.sum(0)
+        h = svds(net[:, sorted(range(n), key=lambda k: sumcol[0, k], reverse=True)[0:min(10*d, n)]], d)[0]
+    block = min(int(ceil(float(n) / splitnum)), 7575)  # Treat at least each 7575 nodes as a block
+    with np.errstate(divide='ignore'):  # inf will be ignored
+        attri = attri.transpose() * sparse.diags(np.ravel(np.power(attri.power(2).sum(1), -0.5)))
+    z = h.copy()
+    affi = -1  # Index for affinity matrix sa
+    u = np.zeros((n, d))
+    '''################# Update functions #################'''
+    def updateh():
+        global affi, sa, h
+        xtx = np.dot(z.transpose(), z) * 2 + rho * np.eye(d)
+        for blocki in range(splitnum):  # Split nodes into different Blocks
+            indexblock = block * blocki  # Index for splitting blocks
+            if affi != blocki:
+                sa = attri[:, range(indexblock, indexblock + min(n - indexblock, block))].transpose() * attri
+                affi = blocki
+            sums = sa.dot(z) * 2
+            for i in range(indexblock, indexblock + min(n - indexblock, block)):
+                neighbor = z[net[:, i].indices, :]  # the set of adjacent nodes of node i
+                for j in range(1):
+                    normi_j = np.linalg.norm(neighbor - h[i, :], axis=1)  # norm of h_i^k-z_j^k
+                    nzidx = normi_j != 0  # Non-equal Index
+                    if np.any(nzidx):
+                        normi_j = (lambd * net[:, i].data)[nzidx] / normi_j[nzidx]
+                        h[i, :] = np.dot(
+                            sums[i - indexblock, :] + (neighbor[nzidx, :] * normi_j.reshape((-1, 1))).sum(0) + rho * (
+                                z[i, :] - u[i, :]),
+                            np.linalg.pinv(xtx + normi_j.sum() * np.eye(d)))
+                    else:
+                        h[i, :] = np.dot(sums[i - indexblock, :] + rho * (
+                            z[i, :] - u[i, :]), np.linalg.pinv(xtx))
+    def updatez():
+        global affi, sa, z
+        xtx = np.dot(h.transpose(), h) * 2 + rho * np.eye(d)
+        for blocki in range(splitnum):  # Split nodes into different Blocks
+            indexblock = block * blocki  # Index for splitting blocks
+            if affi != blocki:
+                sa = attri[:, range(indexblock, indexblock + min(n - indexblock, block))].transpose() * attri
+                affi = blocki
+            sums = sa.dot(h) * 2
+            for i in range(indexblock, indexblock + min(n - indexblock, block)):
+                neighbor = h[net[:, i].indices, :]  # the set of adjacent nodes of node i
+                for j in range(1):
+                    normi_j = np.linalg.norm(neighbor - z[i, :], axis=1)  # norm of h_i^k-z_j^k
+                    nzidx = normi_j != 0  # Non-equal Index
+                    if np.any(nzidx):
+                        normi_j = (lambd * net[:, i].data)[nzidx] / normi_j[nzidx]
+                        z[i, :] = np.dot(
+                            sums[i - indexblock, :] + (neighbor[nzidx, :] * normi_j.reshape((-1, 1))).sum(0) + rho * (
+                                h[i, :] + u[i, :]),
+                            np.linalg.pinv(xtx + normi_j.sum() * np.eye(d)))
+                    else:
+                        z[i, :] = np.dot(sums[i - indexblock, :] + rho * (h[i, :] + u[i, :]),
+                                         np.linalg.pinv(xtx))
+    '''################# First update h #################'''
+    updateh()
+    '''################# Iterations #################'''
+    for iternum in range(maxiter - 1):
+        updatez()
+        u = u + h - z
+        updateh()
+    return h
